be1e73005a1ffdfa1c5587b1623f554111a5e24d
osele <oselesami08@gmail.com>
2023-05-05 11:33:47 +0200

    SQL added

--- www/view/IndexController/loginAktion.tpl.php
+++ www/view/IndexController/loginAktion.tpl.php
@@ -3,14 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <title>Login</title>
-        <link rel="stylesheet" href="src/view/css/style.registry.css">
+        <link rel="stylesheet" href="view/rsrc/css/login.css">
         <link rel="shortcut icon" type="image/x-icon" href="src/rsrc/logo.png" />
         <script src="src/rsrc/js/registry.js"></script>
     </head>
     <body>
-       <form  action="index.php?aktion=login" method="POST" >
+       <form class="login-container" action="index.php?aktion=login" method="POST" >
 
-            <label for="nickname">Nickname<br></label>
+            <label  for="nickname">Nickname<br></label>
             <input class="inputs" type="text" name="nickname" id="nickname" placeholder="Nickname"  required />
 
             <br>
