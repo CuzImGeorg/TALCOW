da8e00c3b2403bc22125ec71a47937160bcbb5ab
Steiner <schnecht555@gmail.com>
2023-06-06 11:45:39 +0200

    installscript

--- www/view/AjaxController/webTerminalAktion.tpl.php
+++ www/view/AjaxController/webTerminalAktion.tpl.php
@@ -1,6 +1,73 @@
-<h1>Terminal</h1>
-<input type="text" id="terminalinput" placeholder="Command">
-<button id="btnterminalexecute" >Execute</button> //TODO
-<br/>
-<textarea id="terminalresults">
-</textarea>
+
+
+
+
+
+
+<div id="terminal"></div>
+
+
+
+<script>
+
+    const terminal = document.getElementById('terminal');
+
+    const inputField = document.createElement('input');
+
+
+
+    const socket = new WebSocket('ws://localhost:3000'); // Replace with your server URL
+
+
+
+    socket.addEventListener('open', () => {
+
+        terminal.innerHTML = 'Connected to server\n';
+
+    });
+
+
+
+    socket.addEventListener('message', (event) => {
+
+        terminal.innerHTML += event.data + '\n';
+
+        terminal.scrollTop = terminal.scrollHeight;
+
+    });
+
+
+
+    const inputListener = (event) => {
+
+        if (event.key === 'Enter') {
+
+            const command = event.target.value;
+
+            event.target.value = '';
+
+
+
+            socket.send(command);
+
+            terminal.innerHTML += '> ' + command + '\n';
+
+            terminal.scrollTop = terminal.scrollHeight;
+
+
+
+            event.preventDefault();
+
+        }
+
+    };
+
+
+
+    inputField.addEventListener('keydown', inputListener);
+
+    document.body.appendChild(inputField);
+
+    inputField.focus();
+
+</script>
